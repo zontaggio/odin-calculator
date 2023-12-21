6d746cf5f4e76debccd5cadd828fea659fb713e7
Zonta <giordano.zonta@gmail.com>
2023-12-20 22:45:56 -0300

    Final adjustments and button C, percentage, signal and dot added.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let operatorFlag = false;
 let operatorArray = [];
 let operatorTypes = ['add', 'subtract', 'multiply', 'divide'];
 let temporaryResult = ''
+let dotFlag = false; // Used to prevent user to user multiple dots inside the same number
 
 // Functions for add, subtract, multiply and divide
 
@@ -16,6 +17,10 @@ const subtract = (firstNumber, secondNumber) => +firstNumber - +secondNumber;
 const multiply = (firstNumber, secondNumber) => +firstNumber * +secondNumber;
 const divide = (firstNumber, secondNumber) => +firstNumber / +secondNumber;
 
+// Locator for the display element
+
+const displayArea = document.querySelector('#display');
+
 // Defining the listeners for buttons 0-9
 
 for (let i = 0; i <= 9; i++) {
@@ -23,13 +28,77 @@ for (let i = 0; i <= 9; i++) {
     button.addEventListener('click', () => {
         if (operatorFlag == false) {
             firstNumber += i;
-            console.log(firstNumber);
+            // console.log(firstNumber);
+            displayArea.innerHTML = firstNumber;
         } else {
             secondNumber += i;
-            console.log(secondNumber);
+            // console.log(secondNumber);
+            displayArea.innerHTML = secondNumber;
+
         }
     });
 };
+
+// Defining specific listener for dot
+const dotButton = document.querySelector('#dot');
+dotButton.addEventListener('click', () => {
+    if (dotFlag == false) {
+        if (operatorFlag == false) {
+            firstNumber = firstNumber + ".";
+            dotFlag = true;
+            // console.log(firstNumber);
+            displayArea.innerHTML = firstNumber;
+        } else {
+            secondNumber = secondNumber + ".";
+            dotFlag = true;
+            // console.log(secondNumber);
+            displayArea.innerHTML = secondNumber;
+        }
+    }
+});
+
+// Defining specific listener for clear
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => {
+    firstNumber = '';
+    secondNumber = '';
+    operatorType = '';
+    lastOperator = '';
+    operatorFlag = false;
+    operatorArray = [];
+    operatorTypes = ['add', 'subtract', 'multiply', 'divide'];
+    temporaryResult = ''
+    dotFlag = false;
+    displayArea.innerHTML = '0';
+});
+
+// Defining specific listener for percentage
+const percentageButton = document.querySelector('#percentage');
+percentageButton.addEventListener('click', () => {
+        if (operatorFlag == false) {
+            firstNumber = firstNumber / 100;
+            // console.log(firstNumber);
+            displayArea.innerHTML = firstNumber;
+        } else {
+            secondNumber = secondNumber / 100;
+            // console.log(secondNumber);
+            displayArea.innerHTML = secondNumber;
+        }
+});
+
+// Defining specific listener for change signal
+const signalButton = document.querySelector('#signal');
+signalButton.addEventListener('click', () => {
+    if (operatorFlag == false) {
+        firstNumber = firstNumber * (-1);
+        // console.log(firstNumber);
+        displayArea.innerHTML = firstNumber;
+    } else {
+        secondNumber = secondNumber * (-1);
+        // console.log(secondNumber);
+        displayArea.innerHTML = secondNumber;
+    }
+});
 
 // Defining the listeners for the operator buttons
 
@@ -43,7 +112,12 @@ operatorTypes.forEach((type) => {
             temporaryResult = operate(firstNumber, operatorArray[operatorArray.length - 2], secondNumber);
             firstNumber = temporaryResult;
             secondNumber = '';
-            return console.log(temporaryResult);
+            // return console.log(temporaryResult);
+            if (temporaryResult % 1 === 0) {
+                displayArea.innerHTML = temporaryResult.toFixed(0);
+            } else {
+                displayArea.innerHTML = temporaryResult.toFixed(9);
+            }
         }
     });
 });
@@ -52,7 +126,13 @@ operatorTypes.forEach((type) => {
 
 const equalButton = document.querySelector('#equal');
 equalButton.addEventListener('click', () => {
-    return console.log(operate(firstNumber, operatorType, secondNumber));
+    // return console.log(operate(firstNumber, operatorType, secondNumber));
+    if ((operate(firstNumber, operatorType, secondNumber)) % 1 == 0) {
+        displayArea.innerHTML = operate(firstNumber, operatorType, secondNumber).toFixed(0);
+    } else {
+        displayArea.innerHTML = operate(firstNumber, operatorType, secondNumber).toFixed(9);
+    }
+
 });
 
 
